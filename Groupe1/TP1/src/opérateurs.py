35cd65457491a46db3a38940b13b8110b395362e
Valentiiine <valentine.paul28@gmail.com>
2024-10-01 16:21:10 +0200

    Update opérateurs.py

--- Groupe1/TP1/src/opérateurs.py
+++ Groupe1/TP1/src/opérateurs.py
@@ -1,25 +1,26 @@
-import math
+import math  # Importation de la bibliothèque mathématique, bien que non utilisée dans ce code
 
-a = 16
-b = 3
+# Initialisation des variables
+a = 16  # Déclaration et initialisation de la variable 'a' avec la valeur 16
+b = 3   # Déclaration et initialisation de la variable 'b' avec la valeur 3
 
-#Addition
-print("Addition (a+b) = ", a+b)
+# Addition
+print("Addition (a+b) = ", a + b)  # Affiche la somme de 'a' et 'b'
 
-#Soustraction
-print("Soustraction (a-b) = ", a-b)
+# Soustraction
+print("Soustraction (a-b) = ", a - b)  # Affiche la différence entre 'a' et 'b'
 
-#Multiplication
-print("Multiplication (axb) = ", a*b)
+# Multiplication
+print("Multiplication (axb) = ", a * b)  # Affiche le produit de 'a' et 'b'
 
-#Division
-print ("Division (a/b) = ", a/b)
+# Division
+print("Division (a/b) = ", a / b)  # Affiche le quotient de 'a' divisé par 'b'
 
-#Division eucidienne
-print("Division euclidienne (a/b) = ", a%b)
+# Division euclidienne
+print("Division euclidienne (a/b) = ", a % b)  # Affiche le reste de la division de 'a' par 'b'
 
-#Egalité
-print("Egalité (a=b) =", a==b)
+# Egalité
+print("Egalité (a=b) =", a == b)  # Affiche True ou False selon que 'a' est égal à 'b'
 
-#Supériorité
-print("a supérieur à b (a>b) =", a>b)
+# Supériorité
+print("a supérieur à b (a>b) =", a > b)  # Affiche True ou False selon que 'a' est supérieur à 'b'
